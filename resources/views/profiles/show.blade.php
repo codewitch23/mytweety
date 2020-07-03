04b115275c563c303933ff3689b8fd22c7891713
majid <majidrad@hotmail.com>
2020-07-03 19:52:12 +0430

    profile ui added

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -6,13 +6,14 @@
             <img src="/img/profile.jpg">
         </div>
         <div class="bx-2">
-            {{$user->name}}
+            <p class="font-weight-bold">{{$user->name}}</p>
+            <p >joined{{$user->created_at->diffForHumans()}}</p>
         </div>
         <div class="bx-3">
             <img src="{{$user->avatar}}" class="rounded-circle">
         </div>
-        <div class="bx-4">
-            <button class="btn btn-style-sm">Edit Profile</button>
+        <div class="bx-4 d-flex">
+            <button class="btn btn-style-sm mr-2">Edit Profile</button>
             <button class="btn  btn-style-outline-sm">Follow Me</button>
         </div>
         <div class="bx-5">
